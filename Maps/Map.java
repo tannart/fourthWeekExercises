3d4ce0eb06e68eacb77a2f99d04347a7a2f88fae
charliembrodie <cbrodi01@cspc052.dcs.bbk.ac.uk>
2012-10-24 15:04:18 +0100

    See goMap.java.

--- Maps/Map.java
+++ Maps/Map.java
@@ -1,35 +1,50 @@
 public class Map implements SimpleMap{
     private Element currentKey;
 	private Element nextKey;
+	private static int elementNumber = 0;
+	
+	public Map(){
+	    Element startElement = new Element(0,"S");
+	    Element nextElement = new Element(1000000,"S");
+		this.currentKey = startElement;
+		this.nextKey = nextElement;
+	}
 	
     public String put(int key, String name){
 	    Element newElement = new Element(key, name);
 		
-		if(currentKey == null){
-		    currentKey = newElement;
-			
-		} else {
-		
-		    currentKey.addElement(newElement);
-			
-		}
-		
-	    return name;
+		currentKey.addElement(newElement);
+
 		System.out.println(name + " has been added to the map");
+		elementNumber++;
+		return name;
 	}
 	
 	public String get(int key){
-	    
+	    System.out.println("Searching...");
+	    String name = currentKey.search(currentKey, key);
+		System.out.println(name + " is at position " + key);
+		return name;
 	}
 	
 	public void remove(int key){
+	    String name = currentKey.search(currentKey, key);
+		System.out.println(name + " has been removed from the map");
+	    String deleted = nextKey.deleteElement(currentKey, key);
+		elementNumber--;
 	}
 	
 	public boolean isEmpty(){
-	if(){
+	    if(currentKey.getNext() == null){
 	    
-		return true;
-	} else{
+		    return true;
+	    } else{
 	    
 		return false;
+	    }
+    }
+	
+	public void getMapDetails(){
+	    System.out.println("There are currently " +elementNumber + " elements in the map");
 	}
+}
