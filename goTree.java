c6440e14cb235ba44a745c2447f0d8717d9db014
Charlie Brodie <charliebrodie@Charlie-Brodies-MacBook-Pro.local>
2012-10-29 21:18:35 +0000

    See IntegerTreeNode.java.

--- goTree.java
+++ goTree.java
@@ -4,27 +4,18 @@ public class goTree{
 	    
 		IntegerTreeNode theTree = new IntegerTreeNode(6);
 		
-		theTree.add(5);
+		/*theTree.add(5);
 		theTree.add(3);
-		theTree.add(9);
-    	theTree.add(10);
-		theTree.add(14);
-		theTree.add(25);
-		theTree.add(36);
-		theTree.add(46);
-		theTree.add(57);
-		theTree.add(35);
+		theTree.add(9);*/
+
 
 
 
 		
 		theTree.contains(532);
 		theTree.contains(66);
-		
-		//theTree.getMax();
-		//theTree.getMin();
-		
+
 		theTree.toString();
 		
 	}
-}	
+}
